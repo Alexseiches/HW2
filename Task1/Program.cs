cad126d44a83156e3d1d3099927aa952862157c6
Алексеев Игорь <dimka_Alexs@mail.ru>
2023-04-01 18:41:27 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int A = int.Parse(Console.ReadLine()!);
+int number = A ;
+if ( number < 100 || number >= 1000 );
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+Console.WriteLine($"Введенное число {number}");
+int SecondNumber = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {SecondNumber}");
---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+int number = A;
+if (A < 100)
+{
+    Console.WriteLine("Не имеет третьей цифры");
+}
+int ThirdNumber(int number)
+{
+    while (number >999)
+    {
+        number /= 10;
+    }
+}
+bool NonNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+int number = Console.ReadLine("Введите число");
+if (NonNumber(number))
+{
+    Console.WriteLine(ThirdNumber(number));
+}
